5aae867524b91659cbbada00d4ddd549f1177d87
490 <zhaole@vip.qq.com>
2018-07-11 16:03:50 +0800

    第32位算不了

---
+++ src/numberof1_15.java
@@ -0,0 +1,39 @@
+public class numberof1_15
+{
+    public static int sum(int num)
+    {
+        int r = 0;
+        int flag=1;
+        while(flag>0)
+        {
+            int x =num&flag;
+            System.out.println("flag="+Integer.toBinaryString(flag));
+
+            System.out.println(Integer.toBinaryString(num)+"___"+x);
+
+            if(x>0)
+            {
+                r++;
+                System.out.println(r);
+            }
+            flag = flag<<1;
+            System.out.println(flag>0);
+            System.out.println("-------------------");
+        }
+        return r;
+    }
+    public static int sum2(int num)
+    {
+        int r=0;
+        while(num>0)
+        {
+            r++;
+            num=(num-1)&num;
+        }
+        return r;
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(sum(0x80000000));
+    }
+}
