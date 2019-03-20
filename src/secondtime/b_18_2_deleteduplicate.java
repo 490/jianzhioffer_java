3ca2c919c0fa35cc1cf9079a15aae1dd4ba5b0ec
zhaole <zhaole@vip.qq.com>
2019-03-20 21:13:10 +0800

    递归删除重复节点。。@_@

---
+++ src/secondtime/b_18_2_deleteduplicate.java
@@ -0,0 +1,49 @@
+package secondtime;
+
+/**
+ * created by zl on 2019/3/20 20:50
+ */
+public class b_18_2_deleteduplicate
+{
+    public listnode delete(listnode head)
+    {
+        if(head == null|| head.next == null)
+            return head;
+        listnode next = head.next;
+        if(head.value == next.value)
+        {
+            while(next != null && head.value == next.value)
+            {
+                next = next.next;
+            }
+            return delete(next);
+        }else{
+            head.next = delete(head.next);
+            return head;
+        }
+    }
+
+    public static void main(String[] args) {
+        b_18_2_deleteduplicate b_18_2_deleteduplicate = new b_18_2_deleteduplicate();
+        listnode list = new listnode();
+
+
+        listnode p1 = list.create(2);
+        listnode p2 = list.create(2);
+        listnode p3 = list.create(3);
+        listnode p4 = list.create(4);
+        listnode p5 = list.create(5);
+        listnode p6 = list.create(5);
+        listnode p7 = list.create(6);
+
+
+        list.connect(p1,p2);
+        list.connect(p2,p3);
+        list.connect(p3,p4);
+        list.connect(p4,p5);
+        list.connect(p5,p6);
+        list.connect(p6,p7);
+        listnode x = b_18_2_deleteduplicate.delete(p1);
+        list.print(x);
+    }
+}
---
+++ src/secondtime/listnode.java
@@ -0,0 +1,64 @@
+package secondtime;
+
+/**
+ * created by zl on 2019/3/20 20:46
+ */
+public class listnode
+{
+    int value;
+    listnode next;
+    public void listnode(int value)
+    {
+        this.value =value;
+        this.next = null;
+    }
+    public int getValue()
+    {
+        return this.value;
+    }
+    public listnode getNext()
+    {
+        return  this.next;
+    }
+
+    public void addToTail(listnode head,int value)
+    {
+        listnode node = new listnode();
+        node.value = value;
+        node.next = null;
+        if(head ==  null)
+            head = node;
+        else{
+            while(head.next != null)
+                head = head.next;
+            head.next = node;
+        }
+    }
+    public listnode create(int value)
+    {
+        listnode node = new listnode();
+        node.next = null;
+        node.value = value;
+        return node;
+    }
+
+    public void connect(listnode cur,listnode p)
+    {
+        if(cur == null)
+            return;
+        cur.next = p;
+    }
+
+    public void print(listnode node)
+    {
+        while(node!=null){
+            System.out.print(node.value+"\t");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+
+
+
+}
